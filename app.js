01cfe873fa910509bc3f1588c586fc01e35d8f49
Uzma Mustafa <49521264+uzmamstf@users.noreply.github.com>
2020-06-21 12:48:05 +0500

    upload initial chps

---
+++ app.js
@@ -0,0 +1,99 @@
+// 1. Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.//////////
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var fullName = firstNmae+ seconfName;
+// document.write("Hello "+fullName +"!");
+
+// 2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+// var favMobModel = prompt("Enter your favorite Mobile Model Name");
+// //var favMobModelLength =  favMobModel.length;
+// document.write("My Favorite Model Of Mobile is: "+ favMobModel  +"!");
+// document.write("Length of Input string is: "+favMobModel.length  +"!");
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+// var nationality = "Pakistani";
+// var extractt = nationality.indexOf("n");
+// document.write(" string is: "+  nationality +"!");
+// document.write("index of 'n' is: "+ extractt);
+
+// 4. Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser
+
+// var greet = "Hello World";
+// var findLastIndex = greet.lastIndexOf("l");
+// document.write("String: "+ greet );
+// document.write("Last Index of L: "+ findLastIndex );
+
+// 5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+
+// var a = "Pakistani";
+// var findChatAt3rd = a.charAt(3);
+// document.write("String: "+ a);
+// document.write("Character at index 3: "+ findChatAt3rd );
+
+// 6. Repeat Q1 using string concat() method.//////
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var fullName = concat(firstName,lastName);
+// document.write("Hello "+fullName +"!");
+
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+
+// var city = "Hyderabad";
+// var newCity = city.replace("Hyder", "Islam")
+// document.write("City: "+ city );
+// document.write("After replacement "+ newCity  );
+
+
+// 8.Write a program to replace all occurrences of “and” in the
+// string with “&” and display the result in your browser.//////////
+
+
+// var message = “Ali and Sami are best friends. They play cricket and
+// football together.”;
+// var replaced = message.replace("and" , "&");
+
+// 9. Write a program that converts a string “472” to a number
+// 472. Display the values & types in your browser.
+
+// var stringValue = "427";
+// newValue = parseInt(stringValue);
+// document.write("Type " + typeof stringValue );
+// document.write("Type " + typeof newValue);
+
+
+// 10. Write a program that takes user input. Convert and
+// show the input in capital letters.
+
+// var userInput= prompt("Enter something here...");
+// userInput.toUpperCase();
+// document.write("User Input"+userInput);
+// document.write("Upper Case"+ userInput.toUpperCase());
+
+
+// 11. Write a program that takes user input. Convert and
+// show the input in title case. //////
+// var inputCase = "javascript";
+ 
+
+// Write a program that converts the variable num to
+// string.
+// var num = 35.36 ;
+// Remove the dot to display “3536” display in your browser.
+
+// var num = 35.36 ;
+// var roundNum = Math.round(num);
